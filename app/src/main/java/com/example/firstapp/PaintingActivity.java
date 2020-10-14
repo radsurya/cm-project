3a003ddd3946f221ea810e32b158e7134478f8ac
Radica Giva <fc44311@alunos.fc.ul.pt>
2020-10-14 21:32:17 +0100

    Assignment 2 added landscape mobile version with fragments

--- app/src/main/java/com/example/firstapp/PaintingActivity.java
+++ app/src/main/java/com/example/firstapp/PaintingActivity.java
@@ -14,25 +14,61 @@ public class PaintingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_painting);
 
-        // getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, firstFragment).commit();
+        View mainFragment = findViewById(R.id.main_fragment);
+        View mainFragment1 = findViewById(R.id.fragment_first_land);
+        View mainFragment2 = findViewById(R.id.fragment_second_land);
 
+        // Set the first fragment by default
+        if (mainFragment != null) {
+            FirstFragment firstFragment = new FirstFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, firstFragment).commit();
+        }
+
+        // Landscape view
+        if (mainFragment1 != null) {
+            FirstFragment firstFragmentLand = new FirstFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_first_land, firstFragmentLand).commit();
+        }
+        if (mainFragment2 != null) {
+            SecondFragment secondFragmentLand = new SecondFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_second_land, secondFragmentLand).commit();
+        }
+
+        // Set the first fragment by clicking in canvas button
         final Button buttonF1 = (Button) findViewById(R.id.button_fragment1);
-        buttonF1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                FirstFragment firstFragment = new FirstFragment();
-                getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, firstFragment).commit();
+        if (buttonF1 != null) {
+            buttonF1.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    FirstFragment firstFragment = new FirstFragment();
+                    View mainFragment = findViewById(R.id.main_fragment);
+                    View fragment2 = findViewById(R.id.fragment_second);
 
-                // your handler code here
-                System.out.println("Teste 1");
+                    if (mainFragment != null) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, firstFragment).commit();
+                    } else if(fragment2 != null) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_second, firstFragment).commit();
+                    }
+                }
+            });
+        }
 
-            }
-        });
+
+        // Set the first fragment by clicking in pallet button
         final Button buttonF2 = (Button) findViewById(R.id.button_fragment2);
-        buttonF2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // your handler code here
-                System.out.println("Teste 2");
-            }
-        });
+        if (buttonF2 != null) {
+            buttonF2.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    SecondFragment secondFragment = new SecondFragment();
+                    View mainFragment = findViewById(R.id.main_fragment);
+                    View fragment1 = findViewById(R.id.fragment_first);
+
+                    if (mainFragment != null) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, secondFragment).commit();
+                    } else if (fragment1 != null) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_first, secondFragment).commit();
+                    }
+                }
+            });
+        }
     }
 }
