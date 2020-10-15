2e540736536f40390fde9ade6ca69e59ec40ee7e
Radica Giva <fc44311@alunos.fc.ul.pt>
2020-10-15 16:23:03 +0100

    Change fragments while change screen orientation portrait to landscape

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -45,6 +46,20 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
         }*/
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen to restart the main acativity
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) { // landscape
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){ // portrait
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 
     /** Called when the user taps the Send button */
