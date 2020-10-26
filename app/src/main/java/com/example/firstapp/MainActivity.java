395a5cf8c320183912ba4a9a3c787ac049d8377e
Radica Giva <fc44311@alunos.fc.ul.pt>
2020-10-26 18:01:12 +0000

    Main activity dded settings and about options top menu and removed unecessary options screen

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.firstapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -44,9 +48,42 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /** Called when the user taps the Send button */
+    /** Called when the user taps the start button */
     public void goToPage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Intent intent = new Intent(this, PaintingActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * Create a menu options on top of the main screen
+     * @param menu - Current menu
+     * @return always true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_menu, menu);
+        return true;
+    }
+
+    /**
+     * Handle menu items
+     * @param item - Current item menu
+     * @return always true
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item1:
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.menu_item2:
+                Intent intent2 = new Intent(this, SettingsActivity.class);
+                startActivity(intent2);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
